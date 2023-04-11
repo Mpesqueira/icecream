383d4b157c03b89bce8a32634b33b681d7d1957f
Mpesqueira <pesqueiramark@gmail.com>
2023-04-11 13:53:15 -0400

    Add variable to title and error mesage for too many flavors

---
+++ process.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * Mark Pesqueira
+ * 04/11/2023
+ * 328/icecream/process.php
+ * Order form for Ice Cream Shoppe
+ */
+
+/*
+ * array(3) {
+  ["flavor"]=>array(2) {
+    [0]=>string(9) "chocolate"
+    [1]=>string(10) "strawberry"
+  }
+  ["cone"]=>string(6) "waffle"
+  ["scoops"]=>string(1) "4"
+}
+ */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Define constants
+define('PRICE_PER_SCOOP', 2.00);
+define('SALES_TAX', 0.08);
+
+
+// Include Header
+$title = "Order Summary";
+include('header.php');
+
+
+?>
+
+
+<body>
+<div class="container">
+    <h1>Thank you for your order!</h1>
+    <?php
+        /*echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";*/
+
+        // Get data from POST array
+        $numScoops = $_POST['scoops'];
+        $cone = $_POST['cone'];
+        $flavors = $_POST['flavor'];
+        $flavorList = implode(", ", $flavors);
+
+        // Make sure flavors does not exceed scoops
+        if (sizeof($flavors) > $numScoops) {
+            echo "<h2>Oops! You have more flavors than scoops.</h2>";
+            return;
+        }
+
+        // Calculate price
+        $subtotal = $numScoops * PRICE_PER_SCOOP;
+        $total = $subtotal + ($subtotal * SALES_TAX);
+
+        // Display summary
+        echo "<p>Number of scoops: $numScoops</p>";
+        echo "<p>Cone selection: $cone</p>";
+        echo "<p>Flavors: $flavorList</p>";
+        echo "<p>Subtotal: $" . number_format($subtotal, 2) . "</p>";
+        echo "<p>Total: $" . number_format($total, 2) . "</p>";
+
+
+    ?>
+</div>
+</body>
+
